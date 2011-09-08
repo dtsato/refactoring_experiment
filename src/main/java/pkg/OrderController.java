d3a2455d639446fc7877bdd06dff1f3f60518ee4
Danilo Sato <dtsato@gmail.com>
2011-09-08 00:57:21 -0400

    'Introduce Field' on checkoutController.getCart() on OrderController

--- src/main/java/pkg/OrderController.java
+++ src/main/java/pkg/OrderController.java
@@ -6,15 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OrderController {
     private final CheckoutController checkoutController;
     private final Order order;
+    private final Cart cart;
 
     public OrderController(CheckoutController checkoutController, Order order) {
         this.checkoutController = checkoutController;
         this.order = order;
+        cart = this.checkoutController.getCart();
     }
 
     @RequestMapping("/place/order")
     public String placeOrder(ModelMap model) {
-        order.place(checkoutController.getCart());
+        order.place(cart);
         model.addAttribute("order", order);
         return "orderPlaced";
     }
