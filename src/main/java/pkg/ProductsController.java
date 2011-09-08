44f473cdcc4ff6ab80c0122ee5b93da2899c8c48
Danilo Sato <dtsato@gmail.com>
2011-09-08 01:40:01 -0400

    'Introduce Field' cart and upsell on ProductsController, initializing on constructor

--- src/main/java/pkg/ProductsController.java
+++ src/main/java/pkg/ProductsController.java
@@ -5,16 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class ProductsController {
     private final CheckoutController checkoutController;
+    private final Cart cart;
+    private final Upsell upsell;
 
     public ProductsController(CheckoutController checkoutController) {
         this.checkoutController = checkoutController;
+        cart = this.checkoutController.getCart();
+        upsell = this.checkoutController.getUpsell();
     }
 
     @RequestMapping("/see/suggested/products")
     public String seeSuggestedProducts(ModelMap model) {
-        checkoutController.getCart().addTo(checkoutController.getUpsell().findUpsell(checkoutController.getCart()));
-        checkoutController.getCart().addTo(checkoutController.getUpsell().findUpsell(checkoutController.getCart()));
-        model.addAttribute("cart", checkoutController.getCart());
+        cart.addTo(upsell.findUpsell(cart));
+        cart.addTo(upsell.findUpsell(cart));
+        model.addAttribute("cart", cart);
         return "suggestedProducts";
     }
 }
