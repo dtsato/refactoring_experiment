ea04398601d4a5c2a92e90c30d4b61f45f4c7a77
Danilo Sato <dtsato@gmail.com>
2011-09-08 01:28:29 -0400

    'Create Field' for parameter upsell

--- src/main/java/pkg/ProductsController.java
+++ src/main/java/pkg/ProductsController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductsController {
     private final CheckoutController checkoutController;
     private final Cart cart;
+    private Upsell upsell;
 
     public ProductsController(CheckoutController checkoutController, Cart cart, Upsell upsell) {
         this.checkoutController = checkoutController;
         this.cart = cart;
+        this.upsell = upsell;
     }
 
     @RequestMapping("/see/suggested/products")
